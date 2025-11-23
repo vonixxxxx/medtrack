7797789ce5446d6937e8858ec80d350a0acf7db7
Alexander Sokol <sharpner21@gmail.com>
2025-11-23 17:17:14 +0200

    fix: update vite resolve.extensions with proper order

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json'],
   },
   plugins: [
     react(),
